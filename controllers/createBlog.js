2444e5c688660adf5ac421899ec7f42adedbdb16
MFA-creator <malakwe.mfa@gmail.com>
2024-04-21 21:08:12 +0000

    blog controllers created

---
+++ controllers/createBlog.js
@@ -0,0 +1,26 @@
+import Blog from '../models/Blog';
+import User from '../models/User';
+import logger from '../utils/logger';
+
+export default const createBlog = async (req, res) => {
+	try {
+		const { title, description, tags, body } = req.body;
+		const authorId = req.user.userId; // Extracted from JWT token
+		const author = await User.findById(authorId);
+		if (!author) {
+			return res.status(404).json({ message: 'Author not found' });
+		}
+		const newBlog = new Blog({
+			title,
+			description,
+			tags,
+			body,
+			author: author._id,
+		});
+		await newBlog.save();
+		res.status(201).json(newBlog);
+	} catch (error) {
+		logger.error('Error in createBlog:', error);
+		res.status(500).json({ message: 'Internal server error' });
+	}
+};
